45921db82733868fd0c4432643e34a87b156037a
Christophe Lauer <c_lauer@hotmail.com>
2017-10-28 21:54:32 +0200

    removed dead code

--- cachematrix.R
+++ cachematrix.R
@@ -65,28 +65,3 @@ cacheSolve <- function(x, ...) {
 ## [1,]    1    3
 ## [2,]    2    4
 
-makeVector <- function(x = numeric()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setmean <- function(mean) m <<- mean
-  getmean <- function() m
-  list(set = set, get = get,
-       setmean = setmean,
-       getmean = getmean)
-}
-
-cachemean <- function(x, ...) {
-  m <- x$getmean()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  data <- x$get()
-  m <- mean(data, ...)
-  x$setmean(m)
-  m
-}
